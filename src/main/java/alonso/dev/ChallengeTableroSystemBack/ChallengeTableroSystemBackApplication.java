cf1357744a9f28ede37631e6e3d4f13f932d45c6
AlonsoDev <marcialpebell@gmail.com>
2025-03-12 20:31:01 -0500

    first commit

---
+++ src/main/java/alonso/dev/ChallengeTableroSystemBack/ChallengeTableroSystemBackApplication.java
@@ -0,0 +1,13 @@
+package alonso.dev.ChallengeTableroSystemBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTableroSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTableroSystemBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alonso/dev/ChallengeTableroSystemBack/ChallengeTableroSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package alonso.dev.ChallengeTableroSystemBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTableroSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
